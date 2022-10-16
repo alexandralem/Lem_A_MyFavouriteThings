6ad2026fde169e60e2c9b796562316e3c5b389cb
Alexandra Lem <a_lem@fanshaweonline.ca>
2022-10-15 23:15:28 -0400

    adding structure

---
+++ js/modules/dataMiner.js
@@ -0,0 +1,14 @@
+function getData(targetURL, callback){
+    console.log(`it's aliive!`);
+
+    fetch(targetURL)
+    .then (res => res.json())
+
+    .then(data => {
+        console.log(data);
+        callback(data);
+
+    })
+    .catch(error => console.log(error));
+}
+export { getData }
